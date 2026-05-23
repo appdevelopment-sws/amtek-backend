4c3ab3387758004903dacf8da48998860253a0ee
Sahil-9619 <sahilkumar242004@gmail.com>
2026-05-23 15:22:26 +0530

    small change

--- modules/dashboard/dashboard.repository.js
+++ modules/dashboard/dashboard.repository.js
@@ -21,7 +21,7 @@ exports.getProviderDashboardStats = async (providerId) => {
     const query = `
         SELECT
             (SELECT COUNT(*) FROM services WHERE provider_id = ?) AS totalServices,
-            (SELECT COUNT(*) FROM customers WHERE provider_id = ?) AS totalCustomers
+            (SELECT COUNT(*) FROM jobs WHERE provider_id = ?) AS totalJobs
     `;
 
     const [rows] = await db.query(query, [
